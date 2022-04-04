62e7dae422aa033eaeb56c1aa31c0b9c9b6795bc
Jakub Trávník <jakub.travnik@gmail.com>
2022-04-04 17:37:17 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 set -e
 
-# Replace environmental variables in multiple files in place. Line per filename.
-echo "$INPUT_FILES" | while IFS= read line ; do sh -c "cat $line | envsubst | sponge $line"; done
+
+if [ -n "$INPUT_VARIABLES" ]; then
+  # Create string limiting variables replaced
+  variables=$(echo "$INPUT_VARIABLES" | tr '\n' ',' | sed -E 's/,+$//g')
+  echo "$INPUT_FILES" | while IFS= read line ; do sh -c "cat $line | envsubst '$variables' | sponge $line"; done
+
+else
+  # Replace environmental variables in multiple files in place. Line per filename.
+  echo "$INPUT_FILES" | while IFS= read line ; do sh -c "cat $line | envsubst | sponge $line"; done
+fi
